e84012c941c6b9e8ace089c77d6cf8e2b174fe01
Jeppe Weikop <jweiko21@student.aau.dk>
2023-10-14 13:01:35 +0200

    Prevent repetition and add randomness to time for reminders

--- src/jobs/reminder.js
+++ src/jobs/reminder.js
@@ -1,18 +1,46 @@
 const db = require('../database/db');
 
+// keep track of the last reminder that was sent
+let lastReminder = '';
+
 const reminderMessages = [
     `Wake up, babes. It's time for <:MiddagsTFT:${process.env.MIDDAGSTFT_EMOJI_ID}>`,
     `<:MiddagsTFT:${process.env.MIDDAGSTFT_EMOJI_ID}>`.repeat(3),
-    `<:MiddagsTFT:${process.env.MIDDAGSTFT_EMOJI_ID}>`.repeat(25),
+    `<:MiddagsTFT:${process.env.MIDDAGSTFT_EMOJI_ID}>`.repeat(24),
     `det tid`
 ]
 
 const getRandomReminder = () => {
-    return reminderMessages[Math.floor(Math.random() * reminderMessages.length)];
+    // filter the last reminder from the list of reminders
+    // this is to ensure the same reminder is not sent two consequentive times
+    const availableReminders = reminderMessages.filter(reminder => reminder !== lastReminder);
+    const reminder = availableReminders[Math.floor(Math.random() * availableReminders.length)];
+
+    // update the lastReminder variable and return the chosen reminder
+    lastReminder = reminder;
+    return reminder;
+}
+
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+  
+const randomDelay = async (minMinutes, maxMinutes) => {
+    // convert to milliseconds
+    const minMillis = minMinutes * 60 * 1000;
+    const maxMillis = maxMinutes * 60 * 1000;
+
+    // sleep for a random number of milliseconds within the defined interval
+    const delayMillis = Math.floor(Math.random() * (maxMillis - minMillis + 1)) + minMillis;
+    await sleep(delayMillis);
 }
 
 // send a gentle reminder that it's time to play MiddagsTFT
-const sendMiddagsTftReminder = (client) => {
+const sendMiddagsTftReminder = async (client) => {
+    // wait for 0-20 minutes just so the reminder isn't sent
+    // the exact same time every day
+    await randomDelay(0, 20);
+
     const channelId = db.getMessageChannel();
     if (!channelId) return;
 
@@ -24,6 +52,6 @@ const sendMiddagsTftReminder = (client) => {
 }
 
 module.exports = {
-    schedule: '0 25 11 * * 1-5',
+    schedule: '0 15 11 * * 1-5',
     execute: sendMiddagsTftReminder
 };
